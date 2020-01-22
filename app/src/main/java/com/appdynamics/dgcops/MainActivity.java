9e2badf292a7091eeb2eaafd1229fc67c92da9b9
Vijay Venkatarajulu <vijay.venkatarajulu@appdynamics.com>
2020-01-22 23:03:50 +0530

    Intermediate

--- app/src/main/java/com/appdynamics/dgcops/MainActivity.java
+++ app/src/main/java/com/appdynamics/dgcops/MainActivity.java
@@ -2,7 +2,13 @@ package com.appdynamics.dgcops;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen1);
+
+        addButtonListener();
     }
+
+    private void addButtonListener() {
+        final Context context = this;
+        Button btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                try {
+                    context.startActivity(new Intent(context, Screen2.class));
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+    }
+
+
 }
---
+++ app/src/main/java/com/appdynamics/dgcops/Screen2.java
@@ -0,0 +1,23 @@
+package com.appdynamics.dgcops;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Screen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen2);
+    }
+
+
+
+
+}
