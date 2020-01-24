f65b99b7a0a4745c7368d14c4bc41021ad41bbc9
pratikmaskey <pratikmaskey@gmail.com>
2020-01-24 10:45:50 +0530

    Notification Fun

--- app/src/main/java/com/appdynamics/dgcops/Screen5a.java
+++ app/src/main/java/com/appdynamics/dgcops/Screen5a.java
@@ -12,8 +12,6 @@ public class Screen5a extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
         setContentView(R.layout.screen5a);
 
 
--- app/src/main/java/com/appdynamics/dgcops/Screen6.java
+++ app/src/main/java/com/appdynamics/dgcops/Screen6.java
@@ -1,5 +1,13 @@
 package com.appdynamics.dgcops;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.view.View;
 
@@ -7,22 +15,71 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Screen6 extends AppCompatActivity {
 
+    NotificationManager notificationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.screen6);
 
+            notificationManager =
+                    (NotificationManager)
+                            getSystemService(Context.NOTIFICATION_SERVICE);
 
+            createNotificationChannel(
+                    "Controller Down",
+                    "controller_down",
+                    "Controller");
+        }
+
+        protected void createNotificationChannel(String id, String name, String description) {
+
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel =
+                    new NotificationChannel(id, name, importance);
+
+            channel.setDescription(description);
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(
+                    new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        public void sendNotification(View view) {
+
+        int notificationID = 101;
+        String channelID = "Controller Down";
+
+            Intent resultIntent = new Intent(this, Screen5a.class);
+
+            PendingIntent pendingIntent =
+                    PendingIntent.getActivity(
+                            this,
+                            0,
+                            resultIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+        final Icon icon = Icon.createWithResource(Screen6.this,
+                android.R.drawable.ic_dialog_info);
+
+        Notification.Action action =
+                new Notification.Action.Builder(icon, "Open", pendingIntent)
+                        .build();
+
+        Notification notification =
+                new Notification.Builder(Screen6.this,
+                        channelID)
+                        .setContentTitle("Controller Critical")
+                        .setContentText("Open notification for more details")
+                        .setSmallIcon(R.drawable.appd_invert_2)
+                        .setChannelId(channelID)
+                        .setContentIntent(pendingIntent)
+                        .setActions(action)
+                        .build();
+
+        notificationManager.notify(notificationID, notification);
     }
 
-
-
-
-
-
-
-
-}
+    }
